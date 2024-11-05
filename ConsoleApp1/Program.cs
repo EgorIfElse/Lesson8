ba827b85ae679e3f448f2a50f2c6d99dfb6022da
user <user@WIN-PRBG4ODTMQN>
2024-11-05 22:31:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp\Lesson8.txt";
+            Random r = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(r.Next(-10, 10));
+                }
+            }
+            int k = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (Convert.ToInt32(sr.ReadLine()) > 0)
+                    {
+                        k++;
+                          
+                    }
+                }
+                Console.WriteLine(k);
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp";
+            string[] files=Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+        }
+    }
+}
